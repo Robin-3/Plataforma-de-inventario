d10cf0fb2dac4352b5714cc7fc7818809a2bc339
luchogiraldo <78859822+luchogiraldo@users.noreply.github.com>
2021-10-11 22:32:20 -0500

    Métodos agregados

--- app.py
+++ app.py
@@ -1,59 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET'])
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/usuarios')
+@app.route('/usuarios', methods=['GET'])
 def usuarios():
     return render_template('usuarios.html')
 
-@app.route('/usuariosAgregar')
+@app.route('/usuarios/agregar', methods=['GET','POST'])
 def usuariosAgregar():
+    if request.method =="POST":
+        return "Aquí se van a agregar los datos de un nuevo usuario"
     return render_template('usuariosAgregar.html')
 
-@app.route('/usuariosEditar')
+@app.route('/usuarios/editar', methods=['GET','PUT'])
 def usuariosEditar():
+    if request.method == "PUT":
+        return "Aquí se van a modificar los datos del usuario seleccionado"
     return render_template('usuariosEditar.html')
 
-@app.route('/usuariosEliminar')
+@app.route('/usuarios/eliminar', methods=['GET','DELETE'])
 def usuariosEliminar():
     return render_template('usuariosEliminar.html')
 
-@app.route('/productos')
+@app.route('/productos', methods=['GET'])
 def productos():
     return render_template('productos.html')
 
-@app.route('/productosAgregar')
+@app.route('/productos/agregar', methods=['GET','POST'])
 def productosAgregar():
     return render_template('productosAgregar.html')
 
-@app.route('/productosEditar')
+@app.route('/productos/editar', methods=['GET','PUT'])
 def productosEditar():
     return render_template('productosEditar.html')
 
-@app.route('/productosEliminar')
+@app.route('/productos/eliminar', methods=['GET','DELETE'])
 def productosEliminar():
     return render_template('productosEliminar.html')
 
-@app.route('/proveedores')
+@app.route('/proveedores', methods=['GET'])
 def proveedores():
     return render_template('proveedores.html')
 
-@app.route('/proveedoresAgregar')
+@app.route('/proveedores/agregar', methods=['GET','POST'])
 def proveedoresAgregar():
     return render_template('proveedoresAgregar.html')
 
-@app.route('/proveedoresEditar')
+@app.route('/proveedores/editar', methods=['GET','PUT'])
 def proveedoresEditar():
     return render_template('proveedoresEditar.html')
 
-@app.route('/proveedoresEliminar')
+@app.route('/proveedores/eliminar', methods=['GET','DELETE'])
 def proveedoresEliminar():
     return render_template('proveedoresEliminar.html')
